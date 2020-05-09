a84cb72070f5ac255c025966659de3b8999d4345
jsyczzh <jsyczzh@sjtu.edu.cn>
2020-05-09 18:34:32 +0800

    Modified

---
+++ controller/create_topo.py
@@ -0,0 +1,53 @@
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSSwitch
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel
+import json
+import sys, os
+sys.path.append(os.getcwd())
+print(sys.path)
+from SFlow.enable_sflow import EnableSFlow
+from Flow.base_net import BaseNet
+
+
+def topology(remoteip, ofversion, f = 'config/topo.json'):
+    net = BaseNet(controller=RemoteController,switch=OVSSwitch)
+    c1 = net.addController("c1",controller=RemoteController,ip=remoteip,port=6653)
+    switch_dict = {}
+    switch_list = []
+    switch_port = {}
+    with open(f, 'r') as load_f:
+        load_dict = json.load(load_f)
+    for pod in load_dict['pod_list']:
+        sw = net.addSwitch(pod['name'], protocols=ofversion)
+        switch_list.append(sw)
+        switch_dict[pod['name']] = sw
+        switch_port[pod['name']] = 0
+        for host in pod['host_list']:
+            h = net.addHost(host['name'], ip=host['ip'])
+            switch_port[pod['name']] += 1
+            net.addLink(switch_dict[pod['name']], h, switch_port[pod['name']])
+    for link in load_dict['link_graph']:
+        switch_port[link['begin']] += 1
+        switch_port[link['end']] += 1
+        print link
+        net.addLink(switch_dict[link['begin']], switch_dict[link['end']], switch_port[link['begin']], switch_port[link['end']])
+    
+    print("***Building network.")
+    net.build()
+    
+    print("***Starting network")
+    net.start()
+     
+    # EnableSFlow(net, sampling_rate=10, polling_rate=10)
+    # net.mouse_elephant_flow()
+    CLI(net)
+    	
+    print("***Stoping network")
+    net.stop()
+
+if __name__ == "__main__":
+    setLogLevel("info")
+    topology("127.0.0.1","OpenFlow15")
+    
--- controller/f10.py
+++ controller/f10.py
@@ -13,7 +13,7 @@ import networkx as nx
 import time, json, sys, os
 
 sys.path.append(os.getcwd())
-
+print(sys.path)
 from topo.ABTree import ABTree
 
 K = 4
@@ -60,15 +60,13 @@ class F10Switch(app_manager.RyuApp):
         # correctly.  The bug has been fixed in OVS v2.1.0.
         n = K // 2
 
-        # For switches 1 to K are downlink ports
-        # K+1 to 2K are uplink ports
-        # 2 * k port for sending down
-        # 2 * k - 1 port for sending up
+        # For switches 1 to n are downlink ports
+        # n+1 to K are uplink ports
         if layer == 1:
             # Action group 1: 
             # Received from hosts, send to uplinks
             buckets = []
-            for i in range(K + 1, 2 * K + 1, 2):
+            for i in range(n + 1, K + 1):
                 bucket_action = [parser.OFPActionOutput(i)]
                 buckets.append(parser.OFPBucket(
                             weight=1,
@@ -115,7 +113,6 @@ class F10Switch(app_manager.RyuApp):
                         self.network[datapath.id][dpid]['port1'],
                         self.network[datapath.id][dpid]['port2']
                         )
-                    out_port = out_port * 2 - 1
                     actions = [parser.OFPActionOutput(out_port)]
                     self.add_flow(datapath, 1, match, actions)
 
@@ -132,30 +129,30 @@ class F10Switch(app_manager.RyuApp):
             # Common Situation: receive packet from L1 switches
             # Randomly distribute flows to uplinks
             # If one uplink fail, go to the other
-            for i in range(K + 1, 2 * K + 1, 2):
+            for i in range(n + 1, K + 1):
                 buckets = [parser.OFPBucket(
                             actions = [parser.OFPActionOutput(i)],
                             watch_port = i
                         )]
                 
-                for j in range(1, n):
-                    out_port = (j * 2 + i) % K + K
+                for j in range(1, n + 1):
+                    out_port = (i + j) % (K + 1) + n + 1
                     buckets.append(
                         parser.OFPBucket(
-                            actions = [parser.OFPActionOutput(j)],
-                            watch_port = j
+                            actions = [parser.OFPActionOutput(out_port)],
+                            watch_port = out_port
                         )
                     )
                 
                 req = parser.OFPGroupMod(
-                    datapath, ofproto.OFPGC_ADD, ofproto.OFPGT_FF, (i + 1) // 2, 
+                    datapath, ofproto.OFPGC_ADD, ofproto.OFPGT_FF, i, 
                     buckets
                 )
                 datapath.send_msg(req)
 
             buckets = []
-            for i in range(K + 2, 2 * K + 1, 2):
-                bucket_action = [parser.OFPActionGroup(i // 2)]
+            for i in range(n + 1, K + 1):
+                bucket_action = [parser.OFPActionGroup(i)]
                 buckets.append(parser.OFPBucket(
                     weight = 1,
                     actions = bucket_action
@@ -166,7 +163,7 @@ class F10Switch(app_manager.RyuApp):
             )
             datapath.send_msg(req)
 
-            for i in range(1, K + 1, 2):
+            for i in range(1, n + 1):
                 match = parser.OFPMatch(in_port = i)
                 actions = [parser.OFPActionGroup(0)]
                 self.add_flow(datapath, 1, match, actions, table_id = 1)
@@ -174,18 +171,18 @@ class F10Switch(app_manager.RyuApp):
             # Receive packets from L3 switches
             # If received from uplinks, meaning a failure in a L3-L2 link
             # Send to next uplink
-            for i in range(1, K + 1, 2):
-                match = parser.OFPMatch(in_port = i + K + 1)
-                actions = [parser.OFPActionOutput(K + ((i + 2) % K))]
+            for i in range(1, n + 1):
+                match = parser.OFPMatch(in_port = i + n)
+                actions = [parser.OFPActionOutput((i + n) % K  + n + 1)]
                 self.add_flow(datapath, 1, match, actions, table_id = 1)
 
         if layer == 3:
             # If no failure, send to the other link
             # If one link failed, send back
-            for in_port in range(1, K + 1, 2):
+            for in_port in range(1, n + 1):
                 buckets = []
-                for i in range(1, n + 1):
-                    out_port = (in_port + 2 * i) % K
+                for i in range(0, n):
+                    out_port = (in_port + i) % n + 1
                     if out_port == in_port:
                         buckets.append(parser.OFPBucket(
                             actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT)],
@@ -197,14 +194,14 @@ class F10Switch(app_manager.RyuApp):
                             watch_port = out_port
                         ))
                 req = parser.OFPGroupMod(
-                    datapath, ofproto.OFPGC_ADD, ofproto.OFPGT_FF, (in_port + 1) // 2, 
+                    datapath, ofproto.OFPGC_ADD, ofproto.OFPGT_FF, in_port, 
                     buckets
                 )
                 datapath.send_msg(req)
 
-            for in_port in range(1, K + 1, 2):
+            for in_port in range(1, n + 1):
                 match = parser.OFPMatch(in_port = in_port)
-                actions = [parser.OFPActionGroup((in_port + 1) // 2)]
+                actions = [parser.OFPActionGroup(in_port)]
                 
                 self.add_flow(datapath, 1, match, actions)
                                 
